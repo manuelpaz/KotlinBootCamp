eda63f9bce829fca0948079ac792a4419bfe21cf
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-10-20 15:12:20 -0500

    Se agrega un data class Decorations y se hace una instancia de esta en makeDecorations y se imprimen todas sus propiedades

--- src/Aquarium/Decorations/Decorations.kt
+++ src/Aquarium/Decorations/Decorations.kt
@@ -5,6 +5,16 @@ fun main(args: Array<String>){
 }
 
 fun makeDecorations(){
-
+// 1.1
+    val d1 = Decorations("granite")
+    println(d1)
 }
 
+// A menudo tenemos clase que principalmente actuan como contenedores de datos. En Kotlin, para las clases que principalmente almacenan datos hay una clase
+//llamada data class con beneficios. Se agrego un package dentrode Aquarium llamado Decorations y en este un archivo llamado también Decorations que es en el que nos
+//encontramos se ha creado una función main y una función makeDecorations
+
+// 1.- Creamos una data class Decorations que sepa acerca de decoraciones de acuarios se puede entonces crear una instancia de esta clase en makeDecorations e imprimir
+//todas sus propiedades
+
+data class Decorations(val rocks: String){}
