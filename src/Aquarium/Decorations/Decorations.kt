1133bb7e2f3f40ce671a1885e4c7ba575770bd23
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-10-20 16:00:45 -0500

    Se muestra el uso del metodo copy de data class que crea un objeto con los mismos valores de las propiedades.

--- src/Aquarium/Decorations/Decorations.kt
+++ src/Aquarium/Decorations/Decorations.kt
@@ -10,15 +10,21 @@ fun makeDecorations(){
     println(d1)
 
     // 2.- Creamos dos mas decoraciones en makeDecorations d2 con "slate" y d3 también con "slate"
-    val d2 = Decorations("salte")
+    val d2 = Decorations("slate")
     println(d2)
 
     val d3 = Decorations("slate")
     println(d3)
 
     // 3.- Agregamos dos instruncciones println que compare d1 con d2 y d2 con d3
-    println(d1.equals(d2))
-    println(d2.equals(d3))
+    println(d1.equals(d2)) // Esto imprimira false
+    println(d2.equals(d3)) // Esto imprimira true
+
+    // 4.- Se pueden copiar data objects utilizando el metodo copy esto crea un nuevo objeto con los mismos valores de las propiedades
+
+    val d4: Decorations = d3.copy()
+    println(d3) //Esto imprimira Decorations(rocks = slate)
+    println(d4) //Esto imprimira Decorations(rocks = slate)
 }
 
 // A menudo tenemos clase que principalmente actuan como contenedores de datos. En Kotlin, para las clases que principalmente almacenan datos hay una clase
