9c55baa236eb461681bda734208037e78ab5f71d
Manuel Paz Robles <manuelpazrobles@gmail.com>
2018-10-20 19:09:44 -0500

    Se muestra el codigo de practica de los data class

--- src/simplSpice/main.kt
+++ src/simplSpice/main.kt
@@ -1,5 +1,7 @@
 package simplSpice
 
+import Spices.Curry
+
 fun main(args: Array<String>){
     buildSimpleSpice()
 }
@@ -19,4 +21,16 @@ fun buildSimpleSpice(){
     val spice = Spice("Cayenne", "spicy")
 
     var lessThanSpicy = listOfSpices.filter { it.heat < 5 }
+
+    val spiceCabinet = listOf(SpiceContainer(Spice("curry", "mild")),
+            SpiceContainer(Spice("green curry", "medium")),
+            SpiceContainer(Spice("red curry", "medium")))
+    for(element in spiceCabinet) println(element.label)
+}
+/*Practice Time
+Create a simple data class, SpiceContainer, that holds one spice.
+Give SpiceContainer a property, label, that is derived from the name of the spice.
+Create some containers with spices and print out their labels.*/
+data class SpiceContainer(val spice: Spice){
+    val label = spice.name
 }
